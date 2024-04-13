6a0ba919a2ff8bf7698e578810c2cc506be759c9
Karol <kpulawski13@gmail.com>
2024-04-13 21:56:25 +0100

    Add dropdown state variables and display it

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react';
 import './components/styles/app.css';
 import Indicator from './components/Indicator';
 import { v4 as uuidv4 } from 'uuid';
+import Dropdown from './components/Dropdown';
 
 function App() {
   const [indicators, setIndicators] = useState([]);
+  const [dropdownPosition, setDropdownPosition] = useState({ x: 0, y: 0 });
+  const [isDropdownOpen, setIsDropdownOpen] = useState([]);
+
+  function showDropdown(x, y) {
+    setDropdownPosition({ x, y });
+    setIsDropdownOpen(true);
+  }
 
   function createIndicator(x, y) {
     const id = uuidv4();
@@ -24,10 +32,18 @@ function App() {
   function handleOnImageClick(event) {
     if (event.target) {
       const rect = event.target.getBoundingClientRect();
-      const x = event.clientX - rect.left;
-      const y = event.clientY - rect.top;
+      // const x = event.clientX - rect.left;
+      // const y = event.clientY - rect.top;
 
+      console.log(event.clientX + 50 > rect.right, rect.right);
+      // Makes sure it doesn't fall off the screen
+      const dropdownY =
+        event.clientY + 140 > rect.bottom ? event.pageY - 150 : event.pageY;
+
+      const dropdownX =
+        event.clientX + 140 > rect.right ? event.pageX - 100 : event.pageX + 40;
       createIndicator(event.pageX, event.pageY);
+      showDropdown(dropdownX, dropdownY);
     }
   }
 
@@ -39,6 +55,9 @@ function App() {
           className="image"
           src="https://i.imgur.com/qnHGiJ8.jpeg"
         />
+        {isDropdownOpen && (
+          <Dropdown x={dropdownPosition.x} y={dropdownPosition.y} />
+        )}
         {indicators.map((indicator) => (
           <Indicator
             key={indicator.id}
