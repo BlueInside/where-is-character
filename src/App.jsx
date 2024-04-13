7ff060fe9156a71c2b62668aff32f13bcbb85035
Karol <kpulawski13@gmail.com>
2024-04-13 18:26:20 +0100

    Console logs XY coords relative to the viewport

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from 'react';
+import './components/styles/app.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const imageRef = useRef(null);
+
+  function handleOnImageClick(event) {
+    const rect = imageRef.current.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    console.log('X coordinate relative to the image:', x);
+    console.log('Y coordinate relative to the image:', y);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="image-container">
+        <img
+          onClick={handleOnImageClick}
+          ref={imageRef}
+          className="image"
+          src="https://i.imgur.com/qnHGiJ8.jpeg"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
