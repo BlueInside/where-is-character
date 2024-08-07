930512265996d66db042059a9a95d007e8e5666c
Karol <kpulawski13@gmail.com>
2024-08-07 11:00:19 +0100

    remove fetch image

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,45 +1,13 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import PropTypes from 'prop-types';
 
 export default function Level({ imgRef, handleOnImageClick }) {
-  const [imageSrc, setImageSrc] = useState('');
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const fetchImage = async () => {
-      try {
-        const response = await axios.get(
-          'https://where-is-character-back-end-production.up.railway.app/level1',
-          {
-            responseType: 'blob',
-            signal: controller.signal,
-            withCredentials: true,
-          }
-        );
-
-        const imageUrl = URL.createObjectURL(response.data);
-        setImageSrc(imageUrl);
-      } catch (error) {
-        throw new Error('Error fetching image: please try again later');
-      }
-    };
-
-    fetchImage();
-
-    return () => {
-      controller.abort();
-      console.log('Fetching aborted');
-    };
-  }, []);
-
   return (
     <div className="image-container">
       <img
         onClick={handleOnImageClick}
         className="image"
         ref={imgRef}
-        src={imageSrc}
+        src={`https://res.cloudinary.com/dhjzutfu9/image/upload/v1723020373/level1_aublme.jpg`}
       />
     </div>
   );
